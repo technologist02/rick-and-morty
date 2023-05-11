2b923740e01f9b46d1d2b004ffea6880df02f3ef
dmitr <dmitry9104@outlook.com>
2023-05-11 21:16:34 +0300

    321

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/rick-and-morty'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
